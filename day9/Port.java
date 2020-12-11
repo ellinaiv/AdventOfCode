2256d357e7cf351fcd2cad25fecde52433cf4e2f
ellinaiv <51704876+ellinaiv@users.noreply.github.com>
2020-12-12 00:42:25 +0100

    Adding part 2, day9

--- day9/Port.java
+++ day9/Port.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 public class Port {
@@ -35,10 +34,50 @@ public class Port {
             long sum = Long.valueOf(int_data[i]);
             if (!valid(preamble, sum)) {
                 System.out.println("Not valid port nr: " + int_data[i]);
+                find_contiguous(int_data, int_data[i]);
                 return;
             }
         }
         System.out.println("All ports are valid");
+
+    }
+
+    public static void find_contiguous(long[] all_ports, long port){
+
+        ArrayList<Long> cont_num = new ArrayList<Long>();
+        long sum = 0;
+        int start_index = 1;
+        int loop = 0;
+        for(int i = 0; i <= all_ports.length; i++){
+            cont_num.add(all_ports[i]);
+            sum += all_ports[i];
+            if(sum == port){
+                cont_num.sort(null);
+                long ret = cont_num.get(cont_num.size() - 1) + cont_num.get(0);
+                System.out.println("Res: " + ret);
+                return;
+            }
+
+            else if(sum > port){
+                loop ++;
+                i = start_index;
+                start_index++;
+                sum = 0;
+                cont_num = new ArrayList<Long>();
+            }
+            else if(loop == all_ports.length){
+                System.out.println("no contiguous intervals detected");
+                return;
+            }
+        }
+
+
+        /*
+        LongStream stream = Arrays.stream(all_ports);
+        List<Long> data = stream.boxed().collect(Collectors.toList());
+
+        Optional<Long> max = data.stream().filter(x -> (x != element && (x + element == port))).max(Long::compare);
+        Optional<Long> min = data.stream().filter(x -> (x != element && (x + element == port))).min(Long::compare);*/
 
     }
     public static boolean valid(List<Long> data, long sum){
@@ -48,6 +87,7 @@ public class Port {
             long filtred_elems = data.stream().filter(x -> (x != element && (x + element == sum))).count();
 
             if(filtred_elems != 0){
+
                 return true;
             }
         }
